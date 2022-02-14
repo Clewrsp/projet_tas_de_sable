2638198d26758dc0b3d4e6d2d2eea5986ba72de1
Clément <clement.crespin@ens.uvsq.fr>
2022-02-14 15:22:41 +0100

    Interface button + canvas

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -6,6 +6,33 @@
 # https://github.com/uvsq-info/projet_tas_de_sable
 #########################################
 
+#### Import librairies ####
+
 import tkinter as tk
+import random as rd
 
 
+#### CREATION DE LA FENÊTRE ####
+racine = tk.Tk()
+racine.title("Mon dessin")
+WIDTH = 500
+HEIGHT = 500
+
+
+#### CREATION DE LA FONCTION BOUTON ALEATOIRE ####
+
+
+
+
+#### CREATION DU BOUTON ####
+bouton_aleatoire = tk.Button(racine, text = "Configuration Aléatoire")
+
+#### CREATION DU CONVAS ####
+canvas = tk.Canvas(racine, width = WIDTH, height = HEIGHT, bg = "black")
+
+#### AFFICHAGE ####
+bouton_aleatoire.grid(column=1, row = 0)
+canvas.grid(column=1, row=1, rowspan = 3)
+
+#### 
+racine.mainloop()
